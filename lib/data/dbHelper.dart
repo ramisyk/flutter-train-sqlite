f8eb3476b9c415d1575b0f4f6ef724d4a6a440e6
Ramis Yuksel <ramisyk97@gmail.com>
2020-09-03 10:42:27 +0300

    sqflite CRUD operations are written

--- lib/data/dbHelper.dart
+++ lib/data/dbHelper.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_demo/models/Product.dart';
 
 class DbHelper {
 
@@ -20,5 +21,30 @@ class DbHelper {
 
   void createDb(Database db, int version) async{
     await db.execute("Create table products(id integer primary key, name text, description text, unitPrice integer)");
+  }
+
+  Future<List<Product>> getProducts() async{
+    Database db = await this.db;
+    var result = await db.query("products");
+    return List.generate(result.length, (i){
+      return Product.fromObject(result[i]);
+    });
+  }
+
+  Future<int> insert(Product product) async{
+    Database db = await this.db;
+    var result = await db.insert("products", product.toMap());
+  }
+
+  Future<int> delete(int id) async{
+    Database db = await this.db;
+    var result = await db.rawDelete("delete from products where id=$id");
+    return result;
+  }
+
+  Future<int> update(Product product) async{
+    Database db = await this.db;
+    var result = await db.update("products", product.toMap(), where: "id=?", whereArgs: [product.id]);
+    return result;
   }
 }
--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -6,4 +6,22 @@ class Product{
 
   Product({this.name, this.description, this.unitPrice});
   Product.withId({this.id, this.name, this.description, this.unitPrice});
+
+  Map<String,dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map["name"] = name;
+    map["description"] = description;
+    map["unitPrice"] = unitPrice;
+
+    if(id != null){
+      map["id"] = id;
+    }
+  }
+
+  Product.fromObject(dynamic o) {
+    this.id = int.tryParse(o["id"]);
+    this.name = o["name"];
+    this.description = o["description"];
+    this.unitPrice = double.tryParse(o["unitPrice"]);
+  }
 }
